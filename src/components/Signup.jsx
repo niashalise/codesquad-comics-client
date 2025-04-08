2df32e82d6bab0a537bf474d366654ef81d306c4
Nia <shalise.nia@gmail.com>
2025-04-08 11:23:52 -0400

    Updated Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,7 +2,10 @@ function Signup({ user, setUser }) {
     const handleFormSubmit = (e) => {
         e.preventDefault();
         console.log("Default prevented.");
-        console.log(e.input.value);
+        console.log(e.target.firstName.value);
+        console.log(e.target.lastName.value);
+        console.log(e.target.email.value);
+        console.log(e.target.password.value);
     };
 
     return (
